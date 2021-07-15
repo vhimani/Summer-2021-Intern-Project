ca61417973b52462cbad12dd97c60b13c358f3ae
fmsteemers <74682794+fmsteemers@users.noreply.github.com>
2021-07-15 09:48:19 -0400

    Create TCB-JupyterNotebook-OpenOnSharedDrive.bat
    File to open Jupyter Notebooks on shared TCB drive

---
+++ code/TCB-JupyterNotebook-OpenOnSharedDrive.bat
@@ -0,0 +1,11 @@
+rem UPDATE notebook-dir to the working directory you want to open the notebook in
+rem MAKE SURE THAT YOUR WINDOWS INSTALLATION OF ANACONDA HAS THE PATH PROPERLY SET UP. IF IT DOESNT WORK, CHECK ENVIRONMENT VARIABLES IN WINDOWS
+rem To do this, ensure that the "Path" variable contains the following lines:
+
+rem 	%USERPROFILE%\Anaconda3
+rem	%USERPROFILE%\Library\mingw-w64\bin
+rem 	%USERPROFILE%\Anaconda3\usr\bin
+rem 	%USERPROFILE%\Anaconda3\Library\bin
+rem 	%USERPROFILE%\Anaconda3\Scripts
+
+start jupyter notebook --notebook-dir=I:\Steemers\GitHub
